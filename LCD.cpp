03e49f9219ccd012a357c8b5fb8e113e64459dc4
FMC <FMC@localhost>
2012-02-04 14:55:42 +0100

    Corrected minor bugs and fixed timings for enable toggle so that it is more in-line with slow LCDs.

---
+++ LCD.cpp
@@ -0,0 +1,202 @@
+// ---------------------------------------------------------------------------
+// Created by Francisco Malpartida on 20/08/11.
+// Copyright 2011 - Under creative commons license 3.0:
+//        Attribution-ShareAlike CC BY-SA
+//
+// This software is furnished "as is", without technical support, and with no 
+// warranty, express or implied, as to its usefulness for any purpose.
+//
+// Thread Safe: No
+// Extendable: Yes
+//
+// @file HD44780.h
+// This file implements a basic liquid crystal library that comes as standard
+// in the Arduino SDK.
+// 
+// @brief 
+// This is a basic implementation of the HD44780 library of the
+// Arduino SDK. This library is a refactored version of the one supplied
+// in the Arduino SDK in such a way that it simplifies its extension
+// to support other mechanism to communicate to LCDs such as I2C, Serial.
+// The original library has been reworked in such a way that this will be
+// the base class implementing all generic methods to command an LCD based
+// on the Hitachi HD44780 and compatible chipsets.
+//
+// This base class is a pure abstract class and needs to be extended. As reference,
+// it has been extended to drive 4 and 8 bit mode control, LCDs and I2C extension
+// backpacks such as the I2CLCDextraIO using the PCF8574* I2C IO Expander ASIC.
+//
+//
+// This library is only compatible with Arduino's SDK version 1.0
+//
+// @version API 1.0.0
+//
+// @author F. Malpartida - fmalpartida@gmail.com
+// ---------------------------------------------------------------------------
+#include <stdio.h>
+#include <string.h>
+#include <inttypes.h>
+
+#if (ARDUINO <  100)
+#include <WProgram.h>
+#else
+#include <Arduino.h>
+#endif
+#include <LCD.h>
+
+// CLASS CONSTRUCTORS
+// ---------------------------------------------------------------------------
+// Constructor
+LCD::LCD () 
+{
+   
+}
+
+// PUBLIC METHODS
+// ---------------------------------------------------------------------------
+
+// Common LCD Commands
+// ---------------------------------------------------------------------------
+void LCD::clear()
+{
+   command(LCD_CLEARDISPLAY);             // clear display, set cursor position to zero
+   delayMicroseconds(HOME_CLEAR_EXEC);    // this command is time consuming
+}
+
+void LCD::home()
+{
+   command(LCD_RETURNHOME);             // set cursor position to zero
+   delayMicroseconds(HOME_CLEAR_EXEC);  // This command is time consuming
+}
+
+void LCD::setCursor(uint8_t col, uint8_t row)
+{
+   const byte row_offsetsDef[]   = { 0x00, 0x40, 0x14, 0x54 }; // For regular LCDs
+   const byte row_offsetsLarge[] = { 0x00, 0x40, 0x10, 0x50 }; // For 16x4 LCDs
+   
+   if ( row >= _numlines ) 
+   {
+      row = _numlines-1;    // rows start at 0
+   }
+   
+   // 16x4 LCDs have special memory map layout
+   // ----------------------------------------
+   if ( _cols == 16 && _numlines == 4 )
+   {
+      command(LCD_SETDDRAMADDR | (col + row_offsetsLarge[row]));
+   }
+   else 
+   {
+      command(LCD_SETDDRAMADDR | (col + row_offsetsDef[row]));
+   }
+
+}
+
+// Turn the display on/off
+void LCD::noDisplay() 
+{
+   _displaycontrol &= ~LCD_DISPLAYON;
+   command(LCD_DISPLAYCONTROL | _displaycontrol);
+}
+
+void LCD::display() 
+{
+   _displaycontrol |= LCD_DISPLAYON;
+   command(LCD_DISPLAYCONTROL | _displaycontrol);
+}
+
+// Turns the underline cursor on/off
+void LCD::noCursor() 
+{
+   _displaycontrol &= ~LCD_CURSORON;
+   command(LCD_DISPLAYCONTROL | _displaycontrol);
+}
+void LCD::cursor() 
+{
+   _displaycontrol |= LCD_CURSORON;
+   command(LCD_DISPLAYCONTROL | _displaycontrol);
+}
+
+// Turns on/off the blinking cursor
+void LCD::noBlink() 
+{
+   _displaycontrol &= ~LCD_BLINKON;
+   command(LCD_DISPLAYCONTROL | _displaycontrol);
+}
+
+void LCD::blink() 
+{
+   _displaycontrol |= LCD_BLINKON;
+   command(LCD_DISPLAYCONTROL | _displaycontrol);
+}
+
+// These commands scroll the display without changing the RAM
+void LCD::scrollDisplayLeft(void) 
+{
+   command(LCD_CURSORSHIFT | LCD_DISPLAYMOVE | LCD_MOVELEFT);
+}
+
+void LCD::scrollDisplayRight(void) {
+   command(LCD_CURSORSHIFT | LCD_DISPLAYMOVE | LCD_MOVERIGHT);
+}
+
+// This is for text that flows Left to Right
+void LCD::leftToRight(void) 
+{
+   _displaymode |= LCD_ENTRYLEFT;
+   command(LCD_ENTRYMODESET | _displaymode);
+}
+
+// This is for text that flows Right to Left
+void LCD::rightToLeft(void) 
+{
+   _displaymode &= ~LCD_ENTRYLEFT;
+   command(LCD_ENTRYMODESET | _displaymode);
+}
+
+// This will 'right justify' text from the cursor
+void LCD::autoscroll(void) 
+{
+   _displaymode |= LCD_ENTRYSHIFTINCREMENT;
+   command(LCD_ENTRYMODESET | _displaymode);
+}
+
+// This will 'left justify' text from the cursor
+void LCD::noAutoscroll(void) 
+{
+   _displaymode &= ~LCD_ENTRYSHIFTINCREMENT;
+   command(LCD_ENTRYMODESET | _displaymode);
+}
+
+// Write to CGRAM of new characters
+void LCD::createChar(uint8_t location, uint8_t charmap[]) 
+{
+   location &= 0x7;            // we only have 8 locations 0-7
+   
+   command(LCD_SETCGRAMADDR | (location << 3));
+   
+   for (int i=0; i<8; i++) 
+   {
+      write(charmap[i]);      // call the virtual write method
+   }
+}
+
+// General LCD commands - generic methods used by the rest of the commands
+// ---------------------------------------------------------------------------
+void LCD::command(uint8_t value) 
+{
+   send(value, COMMAND);
+}
+
+#if (ARDUINO <  100)
+void LCD::write(uint8_t value)
+{
+   send(value, DATA);
+}
+#else
+size_t LCD::write(uint8_t value) 
+{
+   send(value, DATA);
+   return 1;             // assume OK
+}
+#endif
---
+++ LiquidCrystal.cpp
@@ -0,0 +1,297 @@
+// ---------------------------------------------------------------------------
+// Created by Francisco Malpartida on 20/08/11.
+// Copyright 2011 - Under creative commons license 3.0:
+//        Attribution-ShareAlike CC BY-SA
+//LiquidCrystal
+// This software is furnished "as is", without technical support, and with no 
+// warranty, express or implied, as to its usefulness for any purpose.
+//
+// Thread Safe: No
+// Extendable: Yes
+//
+// @file LiquidCrystal.cpp
+// This file implements a basic liquid crystal library that comes as standard
+// in the Arduino SDK.
+// 
+// @brief 
+// This is a basic implementation of the LiquidCrystal library of the
+// Arduino SDK. The original library has been reworked in such a way that 
+// this class implements the all methods to command an LCD based
+// on the Hitachi HD44780 and compatible chipsets using the parallel port of
+// the LCD (4 bit and 8 bit).
+//
+// The functionality provided by this class and its base class is identical
+// to the original functionality of the Arduino LiquidCrystal library.
+//
+//
+// This library is only compatible with Arduino's SDK version 1.0
+//
+//
+// @author F. Malpartida - fmalpartida@gmail.com
+// ---------------------------------------------------------------------------
+#include <stdio.h>
+#include <string.h>
+#include <inttypes.h>
+
+#if (ARDUINO <  100)
+#include <WProgram.h>
+#else
+#include <Arduino.h>
+#endif
+#include <LiquidCrystal.h>
+
+// STATIC helper functions
+// ---------------------------------------------------------------------------
+
+
+// CONSTRUCTORS
+// ---------------------------------------------------------------------------
+LiquidCrystal::LiquidCrystal(uint8_t rs, uint8_t rw, uint8_t enable,
+                             uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3,
+                             uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7)
+{
+   init(LCD_8BIT, rs, rw, enable, d0, d1, d2, d3, d4, d5, d6, d7);
+}
+
+LiquidCrystal::LiquidCrystal(uint8_t rs, uint8_t enable,
+                             uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3,
+                             uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7)
+{
+   init(LCD_8BIT, rs, 255, enable, d0, d1, d2, d3, d4, d5, d6, d7);
+}
+
+LiquidCrystal::LiquidCrystal(uint8_t rs, uint8_t rw, uint8_t enable,
+                             uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3)
+{
+   init(LCD_4BIT, rs, rw, enable, d0, d1, d2, d3, 0, 0, 0, 0);
+}
+
+LiquidCrystal::LiquidCrystal(uint8_t rs,  uint8_t enable,
+                             uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3)
+{
+   init(LCD_4BIT, rs, 255, enable, d0, d1, d2, d3, 0, 0, 0, 0);
+}
+
+// PRIVATE METHODS
+// ---------------------------------------------------------------------------
+
+// When the display powers up, it is configured as follows:
+//
+// 1. Display clear
+// 2. Function set: 
+//    DL = 1; 8-bit interface data 
+//    N = 0; 1-line display 
+//    F = 0; 5x8 dot character font 
+// 3. Display on/off control: 
+//    D = 0; Display off 
+//    C = 0; Cursor off 
+//    B = 0; Blinking off 
+// 4. Entry mode set: 
+//    I/D = 1; Increment by 1 
+//    S = 0; No shift 
+//
+// Note, however, that resetting the Arduino doesn't reset the LCD, so we
+// can't assume that its in that state when a sketch starts (and the
+// LiquidCrystal constructor is called).
+// A call to begin() will reinitialize the LCD.
+//
+// init
+void LiquidCrystal::init(uint8_t fourbitmode, uint8_t rs, uint8_t rw, uint8_t enable,
+                         uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3,
+                         uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7)
+{
+   uint8_t i;
+   
+   // Initialize the IO pins
+   // -----------------------
+   
+   _rs_pin = rs;
+   _rw_pin = rw;
+   _enable_pin = enable;
+   
+   _data_pins[0] = d0;
+   _data_pins[1] = d1;
+   _data_pins[2] = d2;
+   _data_pins[3] = d3; 
+   _data_pins[4] = d4;
+   _data_pins[5] = d5;
+   _data_pins[6] = d6;
+   _data_pins[7] = d7;
+   
+   // Initialize the IO port direction to OUTPUT
+   // ------------------------------------------
+   
+   for ( i = 0; i < 4; i++ )
+   {
+      pinMode ( _data_pins[i], OUTPUT );
+   }
+   
+   // Initialize the rest of the ports if it is an 8bit controlled LCD
+   // ----------------------------------------------------------------
+   
+   if ( !fourbitmode )
+   {
+      for ( i = 4; i < 8; i++ )
+      {
+         pinMode ( _data_pins[i], OUTPUT );
+      }
+   }
+   pinMode(_rs_pin, OUTPUT);
+   
+   // we can save 1 pin by not using RW. Indicate by passing 255 instead of pin#
+   if (_rw_pin != 255) 
+   { 
+      pinMode(_rw_pin, OUTPUT);
+   }
+   
+   pinMode(_enable_pin, OUTPUT);
+   
+   // Initialise displaymode functions to defaults: LCD_1LINE and LCD_5x8DOTS
+   // -------------------------------------------------------------------------
+   if (fourbitmode)
+      _displayfunction = LCD_4BITMODE | LCD_1LINE | LCD_5x8DOTS;
+   else 
+      _displayfunction = LCD_8BITMODE | LCD_1LINE | LCD_5x8DOTS;
+}
+
+// PUBLIC METHODS
+// ---------------------------------------------------------------------------
+
+//
+// begin
+void LiquidCrystal::begin(uint8_t cols, uint8_t lines, uint8_t dotsize) 
+{
+   if (lines > 1) 
+   {
+      _displayfunction |= LCD_2LINE;
+   }
+   _numlines = lines;
+   _cols = cols;
+   
+   // for some 1 line displays you can select a 10 pixel high font
+   // ------------------------------------------------------------
+   if ((dotsize != 0) && (lines == 1)) 
+   {
+      _displayfunction |= LCD_5x10DOTS;
+   }
+   
+   // SEE PAGE 45/46 FOR INITIALIZATION SPECIFICATION!
+   // according to datasheet, we need at least 40ms after power rises above 2.7V
+   // before sending commands. Arduino can turn on way before 4.5V so we'll wait 
+   // 50
+   // ---------------------------------------------------------------------------
+   delayMicroseconds(50000); 
+   
+   // Now we pull both RS and R/W low to begin commands
+   digitalWrite(_rs_pin, LOW);
+   digitalWrite(_enable_pin, LOW);
+   
+   if (_rw_pin != 255) 
+   { 
+      digitalWrite(_rw_pin, LOW);
+   }
+   
+   //put the LCD into 4 bit or 8 bit mode
+   // -------------------------------------
+   if (! (_displayfunction & LCD_8BITMODE)) 
+   {
+      // this is according to the hitachi HD44780 datasheet
+      // figure 24, pg 46
+      
+      // we start in 8bit mode, try to set 4 bit mode
+      writeNbits(0x03, 4);
+      delayMicroseconds(4500); // wait min 4.1ms
+      
+      // second try
+      writeNbits(0x03, 4);
+      delayMicroseconds(4500); // wait min 4.1ms
+      
+      // third go!
+      writeNbits(0x03, 4); 
+      delayMicroseconds(150);
+      
+      // finally, set to 4-bit interface
+      writeNbits(0x02, 4); 
+   } 
+   else 
+   {
+      // this is according to the hitachi HD44780 datasheet
+      // page 45 figure 23
+      
+      // Send function set command sequence
+      command(LCD_FUNCTIONSET | _displayfunction);
+      delayMicroseconds(4500);  // wait more than 4.1ms
+      
+      // second try
+      command(LCD_FUNCTIONSET | _displayfunction);
+      delayMicroseconds(150);
+      
+      // third go
+      command(LCD_FUNCTIONSET | _displayfunction);
+   }
+   
+   // finally, set # lines, font size, etc.
+   command(LCD_FUNCTIONSET | _displayfunction);  
+   
+   // turn the display on with no cursor or blinking default
+   _displaycontrol = LCD_DISPLAYON | LCD_CURSOROFF | LCD_BLINKOFF;  
+   display();
+   
+   // clear the LCD
+   clear();
+   
+   // Initialize to default text direction (for romance languages)
+   _displaymode = LCD_ENTRYLEFT | LCD_ENTRYSHIFTDECREMENT;
+   // set the entry mode
+   command(LCD_ENTRYMODESET | _displaymode);
+   
+}
+
+/************ low level data pushing commands **********/
+
+// send
+void LiquidCrystal::send(uint8_t value, uint8_t mode) 
+{
+   digitalWrite( _rs_pin, mode );
+   
+   // if there is a RW pin indicated, set it low to Write
+   // ---------------------------------------------------
+   if (_rw_pin != 255) 
+   { 
+      digitalWrite(_rw_pin, LOW);
+   }
+   
+   if (_displayfunction & LCD_8BITMODE)
+   {
+      writeNbits(value, 8); 
+   } 
+   else 
+   {
+      writeNbits ( value >> 4, 4 );
+      writeNbits ( value, 4 );
+   }
+   waitUsec ( EXEC_TIME ); // wait for the command to execute by the LCD
+}
+
+//
+// pulseEnable
+void LiquidCrystal::pulseEnable(void) 
+{
+   // There is no need for the delays, since the digitalWrite operation
+   // takes longer.
+   digitalWrite(_enable_pin, HIGH);   
+   waitUsec(1);          // enable pulse must be > 450ns   
+   digitalWrite(_enable_pin, LOW);
+   waitUsec(20);
+}
+
+//
+// write4bits
+void LiquidCrystal::writeNbits(uint8_t value, uint8_t numBits) 
+{
+   for (uint8_t i = 0; i < numBits; i++) 
+   {
+      digitalWrite(_data_pins[i], (value >> i) & 0x01);
+   }
+   pulseEnable();
+}
