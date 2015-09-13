4535421d8802989403542d4e634e426f45a9f41c
fmalpartida <fmalpartida@gmail.com>
2015-09-13 21:05:59 +0200

    Inclusion of the SoftI2CMaster library to control I2C devices via bit banging.

--- utility/docs/html/annotated_dup.js
+++ utility/docs/html/annotated_dup.js
@@ -10,5 +10,6 @@ var annotated_dup =
     [ "LiquidCrystal_SR1W", "class_liquid_crystal___s_r1_w.html", "class_liquid_crystal___s_r1_w" ],
     [ "LiquidCrystal_SR2W", "class_liquid_crystal___s_r2_w.html", "class_liquid_crystal___s_r2_w" ],
     [ "LiquidCrystal_SR3W", "class_liquid_crystal___s_r3_w.html", "class_liquid_crystal___s_r3_w" ],
-    [ "SI2CIO", "class_s_i2_c_i_o.html", "class_s_i2_c_i_o" ]
+    [ "SI2CIO", "class_s_i2_c_i_o.html", "class_s_i2_c_i_o" ],
+    [ "SoftI2CMaster", "class_soft_i2_c_master.html", "class_soft_i2_c_master" ]
 ];
--- utility/docs/html/search/classes_2.js
+++ utility/docs/html/search/classes_2.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['si2cio',['SI2CIO',['../class_s_i2_c_i_o.html',1,'']]]
+  ['si2cio',['SI2CIO',['../class_s_i2_c_i_o.html',1,'']]],
+  ['softi2cmaster',['SoftI2CMaster',['../class_soft_i2_c_master.html',1,'']]]
 ];
