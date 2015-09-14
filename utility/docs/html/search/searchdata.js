e118bfc47c2064296eaca2f8b820b557335c3803
fmalpartida <fmalpartida@gmail.com>
2015-09-15 00:51:49 +0200

    Resolved #58 Error in SoftI2CMaster usage. Replaced by: https://github.com/felias-fogg/SoftI2CMaster

--- utility/docs/html/search/searchdata.js
+++ utility/docs/html/search/searchdata.js
@@ -2,20 +2,23 @@ var indexSectionsWithContent =
 {
   0: "abcdhilmnoprsw",
   1: "ils",
-  2: "abcdhilmnoprsw"
+  2: "abcdhilmnoprsw",
+  3: "r"
 };
 
 var indexSectionNames =
 {
   0: "all",
   1: "classes",
-  2: "functions"
+  2: "functions",
+  3: "pages"
 };
 
 var indexSectionLabels =
 {
   0: "All",
   1: "Classes",
-  2: "Functions"
+  2: "Functions",
+  3: "Pages"
 };
 
